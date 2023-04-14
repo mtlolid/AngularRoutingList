3654123eddd3c3390bbd5b2881e1ba2aee3595c1
mtlolid <mtloidmail@gmail.com>
2023-04-14 13:06:15 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './pages/main/main.component';
+import { CenzorMainComponent } from './pages/cenzor-main/cenzor-main.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { TaskInputComponent } from './pages/task-input/task-input.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'main' },
+  { path: 'main', component: MainComponent },
+  { path: 'cenzor', component: CenzorMainComponent },
+  { path: 'userlist', component: UserListComponent },
+  { path: 'tasklist', component: TaskInputComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { MainComponent } from './pages/main/main.component';
+import { CenzorMainComponent } from './pages/cenzor-main/cenzor-main.component';
+import { TaskInputComponent } from './pages/task-input/task-input.component';
+import { ListInputComponent } from './pages/task-input/list-input/list-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserListComponent,
+    MainComponent,
+    CenzorMainComponent,
+    TaskInputComponent,
+    ListInputComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
